3a94f1e319d1941aad44692b1afa91ae2d568778
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-01-20 16:36:52 -0300

    Refactor(module): send real date to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (ConfigService: ConfigService) => (
+      {
+        type: 'postgres',
+        host: ConfigService.get('DB_HOST' , 'localhost'),
+        port: Number(ConfigService.get('DB_PORT', 5432)),
+        username: ConfigService.get('DB_USERNAME','postgres'),
+        password: ConfigService.get('DB_PASSWORD','postgres'),
+        database: ConfigService.get('DB_DATABASE','test-first'),
+        entities: [],
+        synchronize: true,
+      }
+    )
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
