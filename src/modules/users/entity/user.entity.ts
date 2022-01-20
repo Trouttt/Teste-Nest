e3a2aa3225bf181c25cc123eae939743e358b94d
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-01-20 18:48:29 -0300

    Feat(entity): added data in entity

--- src/modules/users/entity/user.entity.ts
+++ src/modules/users/entity/user.entity.ts
@@ -1,6 +1,34 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity({ name: 'users'})
 export class UserEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
+    @Column()
+    name: string;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    cep: string;
+
+    @Column()
+    publicSpace: string;
+
+    @Column()
+    state: string;
+
+    @Column()
+    city: string;
+
+    @CreateDateColumn( { name: 'created_at'} )
+    createdAt:string;
+
+    @UpdateDateColumn( { name: 'updated_at'} )
+    updatedAt: string;
+
+    @DeleteDateColumn( { name: 'deleted_at'} )
+    deletedAt: string;
 }
