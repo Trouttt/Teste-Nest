87c6a132ce17c9fcf45a4deed0544a7a227fb5ef
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-01-23 21:16:24 -0300

    Added(test): added user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entity/user.entity';
+import { cpf } from 'cpf-cnpj-validator'; 
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>){
+
+    }
+
+    async create(data: CreateUserDto) {
+        try{
+            if(cpf.isValid(data.cpf)){
+                return await this.userRepository.save(this.userRepository.create(data))
+            } else {
+                throw new Error();
+            }
+        } catch(error){
+            throw new BadRequestException(error.message = `${cpf.isValid(data.cpf) ? 'CPF is already registered' : 'CPF is invalid'}`);        
+        }
+    }
+    
+}
