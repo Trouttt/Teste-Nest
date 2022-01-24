151c0414ebe508390cebb72af21d533a3ee9cf13
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-01-23 21:39:27 -0300

    Feat(controller): added getAllUsers function in controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, ParseUUIDPipe, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('api/users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService){}
+
+    @Get()
+    async getAllUsers(){
+        return await this.userService.findAll()
+    }
+
+}
